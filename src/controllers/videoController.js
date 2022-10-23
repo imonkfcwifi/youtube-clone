6a256b2ad1821d273aa349315089fb190e9bde06
imonkfcwifi <nohyungkorea@gmail.com>
2022-10-23 22:32:17 +0900

    TAKE MY ENERGY

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,7 @@
 import dideo from "../models/video";
 // video.find({}, (error, videos) => {]
-
+// static 을 이용해서 formatHashtags를 import 하지 않아도 dideo만 import 하면 끝
+// dideo.format~~ 로 써도 됨
 export const homepageVideos = async (req, res) => {
     // synchronous 상황을 해야 await 가능 cuz await은 문법상 funtion에서만 활용가능
     const videos = await dideo.find({});
@@ -26,8 +27,23 @@ export const getEdit = async (req, res) => {
 
 export const postEdit = (req, res) => {
     const { id } = req.params;
+<<<<<<< Updated upstream
     const { title } = req.body;
     console.log(req.body.title);
+=======
+    const { title, description, hashtags } = req.body
+    const video = await dideo.exists({ _id: id });
+    // find mongoose id (property) = const id (any)
+    if (!video) {
+        return res.render("404", { pageTitle: "Video Not Found..!" });
+    }
+    await dideo.findByIdAndUpdate(id, {
+        title,
+        description,
+        hashtags: dideo.formatHashtags(hashtags)
+    })
+
+>>>>>>> Stashed changes
     return res.redirect(`/videos/${id}`);
 }
 
@@ -43,7 +59,7 @@ export const postUpload = async (req, res) => {
                 title,
                 description,
                 createdAt: Date.now(),
-                hashtags: hashtags.split(",").map(word => `#${word}`),
+                hashtags: dideo.formatHashtags(hashtags),
             }
         )
         return res.redirect("/");
@@ -58,3 +74,11 @@ export const postUpload = async (req, res) => {
             })
     }
 };
+
+export const deleteVideo = async (req, res) => {
+    const { id } = req.params;
+    await dideo.findByIdAndDelete(id);
+    // delete video
+    return res.redirect("/");
+
+}
--- src/models/video.js
+++ src/models/video.js
@@ -1,5 +1,13 @@
 import mongoose from "mongoose";
 
+// export const formatHashtags = (hashtags) =>
+//     hashtags.split(",").map
+//     (
+//     (word)=>
+//     (word.startsWith("#")? 
+//     word:`#${word}`)
+//     )
+
 const videoSchema = new mongoose.Schema({
     title: { type: String, required: true, trim: true, maxLength: 20 },
     description: { type: String, required: true, trim: true, maxLength: 150 },
@@ -11,8 +19,24 @@ const videoSchema = new mongoose.Schema({
 
     },
 });
+
+videoSchema.static('formatHashtags', function (hashtags) {
+    return hashtags.split(",").map
+        (
+            (word) =>
+            (word.startsWith("#") ?
+                word : `#${word}`)
+        )
+
+    // // 1. findByIdAndUpdate()에서는 save 훅업이 발생하지 않음 => 다른 방법을 알아보자
+    // 2. Video.js에 function을 만들어서 관리하기 => 이것도 괜찮음 근데 다른것도 알아보자
+    // 3. static을 사용하면 import 없이도 Model.function()형태로 사용이 가능함 => super cool
+})
+
 // The more specific the data / / the better to catch errors
 // db, html both need schma type
+
+
 const dideo = mongoose.model("video", videoSchema);
 
 export default dideo;
