04aa13ea6d9bcd3a2bf7c8031eacaebb40849167
imonkfcwifi <nohyungkorea@gmail.com>
2022-10-26 02:43:48 +0900

    password, hash that password

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcrypt"
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
@@ -9,6 +9,10 @@ const userSchema = new mongoose.Schema({
 
 
 });
+
+userSchema.pre('save', async function () {
+    this.password = await bcrypt.hash(this.password, 5);
+})
 
 const User = mongoose.model('User', userSchema);
 export default User;
