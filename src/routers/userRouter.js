6eb36a7e0db7afc1f45b815f74c115277fcf79cb
imonkfcwifi <nohyungkorea@gmail.com>
2022-11-02 00:57:04 +0900

    token

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userController";
+import { edit, remove, logout, see, startGithubLogin, finishGithubLogin } from "../controllers/userController";
 const userRouter = express.Router();
 
 userRouter.get("/edit", edit);
 userRouter.get("/delete", remove);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get("/:id", see);
 userRouter.get("/logout", logout);
-
 export default userRouter;
