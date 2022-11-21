e8b5518caa2bf704fbf6b129602b3b3267e04052
imonkfcwifi <nohyungkorea@gmail.com>
2022-11-21 12:07:29 +0900

    thumbnail

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,35 +1,57 @@
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
-import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 
 let stream;
 let recorder;
 let videoFile;
 
 const handleDownload = async () => {
-
     const ffmpeg = createFFmpeg({ log: true });
     await ffmpeg.load();
 
     ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
 
-
     await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
 
+    await ffmpeg.run(
+        "-i",
+        "recording.webm",
+        "-ss",
+        "00:00:01",
+        "-frames:v",
+        "1",
+        "thumbnail.jpg"
+    );
 
     const mp4File = ffmpeg.FS("readFile", "output.mp4");
+    const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
 
     const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+    const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
 
     const mp4Url = URL.createObjectURL(mp4Blob);
+    const thumbUrl = URL.createObjectURL(thumbBlob);
+
     const a = document.createElement("a");
     a.href = mp4Url;
     a.download = "MyRecording.mp4";
     document.body.appendChild(a);
     a.click();
-    // download attribute
 
+    const thumbA = document.createElement("a");
+    thumbA.href = thumbUrl;
+    thumbA.download = "MyThumbnail.jpg";
+    document.body.appendChild(thumbA);
+    thumbA.click();
 
+    ffmpeg.FS("unlink", "recording.webm");
+    ffmpeg.FS("unlink", "output.mp4");
+    ffmpeg.FS("unlink", "thumbnail.jpg");
+
+    URL.revokeObjectURL(mp4Url);
+    URL.revokeObjectURL(thumbUrl);
+    URL.revokeObjectURL(videoFile);
 };
 
 const handleStop = () => {
@@ -43,7 +65,7 @@ const handleStart = () => {
     startBtn.innerText = "Stop Recording";
     startBtn.removeEventListener("click", handleStart);
     startBtn.addEventListener("click", handleStop);
-    recorder = new MediaRecorder(stream);
+    recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
     recorder.ondataavailable = (event) => {
         videoFile = URL.createObjectURL(event.data);
         video.srcObject = null;
